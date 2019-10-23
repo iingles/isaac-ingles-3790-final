453bff6c910dd73565a5740d84ba58172d0bf98a
Isaac Ingles <isaac.ingles@gmail.com>
2019-10-23 14:18:45 -0600

    fixed left drawer

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -23,16 +23,28 @@
         </template>
       </v-menu>
     </v-app-bar>
-    <LeftDrawer />
+     <v-navigation-drawer 
+        app
+        fixed
+        v-model="drawer"
+        >
+        <v-btn block to="/">Dashboard</v-btn>
+        <v-btn block to="/servermanagement">Server Management</v-btn>
+        <v-btn block to="/system">System</v-btn>
+        <v-btn block to="/logs">Logs</v-btn>
+        <v-btn block to="/services">Services</v-btn>
+        <v-btn block to="/accounts">Accounts</v-btn>
+        <v-btn block to="/diagnostics">Diagnostics</v-btn>
+        <v-btn block to="/terminal">Terminal</v-btn>
+    </v-navigation-drawer>
   </div>
 </template>
 
 <script>
-import LeftDrawer from "./LeftDrawer"
 
 export default {
   components: {
-    LeftDrawer
+
   },
   data: () => ({
     drawer: null,
