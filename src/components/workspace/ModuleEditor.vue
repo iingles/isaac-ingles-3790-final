94e12aec2365e27a378ec7e734c41bc4213ce162
Isaac Ingles <isaac.ingles@gmail.com>
2019-10-24 15:07:35 -0600

    styled module editor better

--- src/components/workspace/ModuleEditor.vue
+++ src/components/workspace/ModuleEditor.vue
@@ -1,31 +1,37 @@
 <template>
     <v-container>
-        <v-card>
-            <v-card-title>Editing (module name goes here)</v-card-title>
-            <v-form>                
-                <v-row>
-                    <h1>Module Name: (module name)</h1>
-                    <v-checkbox
-                        v-model="changeModuleName"
-                        hide-details
-                        class="shrink mr-2 mt-0"
-                    ></v-checkbox>
-                    <v-text-field
-                        label="Module Name"
-                        value="modulename (will be dynamically added)"
-                        :disabled="!changeModuleName"
-                    ></v-text-field>
-                    <v-checkbox
-                        v-model="changeInternalName"
-                        hide-details
-                        class="shrink mr-2 mt-0"
-                    ></v-checkbox>
-                    <v-text-field
-                        label="Internal (system) Name"
-                        value="internalname (will be dynamically added)"
-                        :disabled="!changeInternalName"
-                    ></v-text-field>
-                </v-row>        
+        <v-card class="pa-10">
+            <v-card-title class="mb-10 pa-0 editor-title">Editing (module name goes here)</v-card-title>
+            <v-form>            
+                <p>Module Name</p>
+                <v-row class="xs-1 lg-1">
+                    <v-col>                     
+                        <v-checkbox
+                            v-model="changeModuleName"
+                            hide-details
+                            class="shrink mr-2 mt-0"
+                        ></v-checkbox>
+                        <v-text-field
+                            label="Change Module Name"
+                            value="modulename (will be dynamically added)"
+                            :disabled="!changeModuleName"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row class="xs-1 lg-1">
+                    <v-col>
+                        <v-checkbox
+                            v-model="changeInternalName"
+                            hide-details
+                            class="shrink mr-2 mt-0"
+                        ></v-checkbox>
+                        <v-text-field
+                            label="Change Internal (system) Name"
+                            value="internalname (will be dynamically added)"
+                            :disabled="!changeInternalName"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>    
             </v-form>
         </v-card>
     </v-container>
@@ -34,12 +40,15 @@
 <script>
 export default {
     data: () => ({
-      includeFiles: true,
-      enabled: false,
+      changeInternalName: false,
+      changeModuleName: false,
     }),
 }
 </script>
 
 <style scoped>
-
+    .editor-title {
+        color: red;
+        border-bottom: 1px dotted #ccc
+    }
 </style>
--- src/views/Workspace.vue
+++ src/views/Workspace.vue
@@ -8,7 +8,20 @@
                 <SideBar />
             </v-col>
             <v-col xs="12" md="9">
-                <router-view></router-view>
+                <ModuleEditor />
+                <!-- <router-view></router-view> -->
+                <!-- <ul>
+                    <router-link
+                        class="module-link"
+                        tag="li"  
+                        v-for="module in modules" 
+                        :key="module.id" 
+                        :to="'/workspace/' + module.name"
+                        style="cursor: pointer"
+                    >
+                        Module {{ module.id }} {{ module.name}}
+                    </router-link>
+                </ul> -->
             </v-col>
         </v-row>
     </v-container>
@@ -17,15 +30,21 @@
 <script>
 import SideBar from '../components/workspace/menus/Sidebar.vue'
 import WorkspaceMenu from '../components/workspace/menus/WorkspaceMenu.vue'
+//do this here for now
+import ModuleEditor from  '../components/workspace/ModuleEditor.vue'
+import contentModules from '../assets/modules.js'
 
 export default {
     components: {
         SideBar,
         WorkspaceMenu,
+        ModuleEditor,
     },
     data: () => {
         return {
-            viewMode: '',
+            //default to content view
+            viewMode: 'content',
+            modules: contentModules,
         }
     },
     methods: {
