2199bb63af6473327ad33ccd5682db3308b01ac2
Isaac Ingles <isaac.ingles@gmail.com>
2019-10-24 13:26:10 -0600

    added settings, about, log

--- src/components/shared/TopRightMenu.vue
+++ src/components/shared/TopRightMenu.vue
@@ -1,9 +1,9 @@
 <template>
     <v-list>
         <v-list-item to="/settings"><v-icon>mdi-settings-outline</v-icon>Settings</v-list-item>
-        <v-list-item to="/logs"><v-icon>mdi-menu_open</v-icon>Logs</v-list-item>
+        <v-list-item to="/logs"><v-icon>mdi-clipboard-text-outline</v-icon>Logs</v-list-item>
         <v-list-item to="/updates"><v-icon>mdi-history</v-icon>Check for Updates</v-list-item>
-        <v-list-item to="/helpform"><v-icon>mdi-help</v-icon>Help</v-list-item>
+        <v-list-item to="/help"><v-icon>mdi-help</v-icon>Help</v-list-item>
         <v-list-item to="/about"><v-icon>mdi-information-outline</v-icon>About</v-list-item>
     </v-list>
 </template>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Dashboard from './views/Dashboard.vue'
+//lazy load these when I get the chance
+import Help from './views/Help.vue'
+import About from './views/About.vue'
+import Settings from './views/Settings.vue'
 
 Vue.use(Router)
 
@@ -10,9 +14,28 @@ export default new Router({
 
   routes: [
     {
+      name: 'root',
+      path: '*',
+    },
+    {
       name: 'Dashboard',
       path: '/',      
       component: Dashboard
+    },
+    {
+      name: 'Settings',
+      path: '/settings',      
+      component: Settings
+    },
+    {
+      name: 'Help',
+      path: '/help',      
+      component: Help
+    },
+    {
+      name: 'About',
+      path: '/about',      
+      component: About
     },
   ]
 
