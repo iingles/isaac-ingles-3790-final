ce311cb108d08a5eaa2e08577d0de3b15c5284ca
Isaac Ingles <isaac.ingles@gmail.com>
2019-10-24 14:32:12 -0600

    Got workspace menu working and removed it from dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,35 +1,15 @@
 <template>
     <v-container>
-        <!-- <v-row>
-            <WorkspaceMenu 
-                @changemode="change($event)"
-            />
-        </v-row>
-        <v-row>
-            <Workspace />
-        </v-row> -->
+        Dashboard
     </v-container>
 </template>
 
 <script>
-// import WorkspaceMenu from '../components/workspace/menus/WorkspaceMenu.vue'
-// import Workspace from '../components/workspace/Workspace.vue'
 
 export default {
     components: {
-        // WorkspaceMenu,
-        // Workspace,
+        
     },
-    data: () => {
-        return {
-            viewMode: '',
-        }
-    },
-    methods: {
-        change(theEvent) {
-            this.viewMode = theEvent
-        }
-    }
 }
 </script>
 
--- src/views/Workspace.vue
+++ src/views/Workspace.vue
@@ -1,5 +1,8 @@
 <template>
      <v-container>
+        <WorkspaceMenu 
+        @changemode="change($event)"
+        />
         <v-row>
             <v-col xs="12" md="3"> 
                 <SideBar />
@@ -13,11 +16,23 @@
 
 <script>
 import SideBar from '../components/workspace/menus/Sidebar.vue'
-
+import WorkspaceMenu from '../components/workspace/menus/WorkspaceMenu.vue'
 
 export default {
     components: {
         SideBar,
+        WorkspaceMenu,
+    },
+    data: () => {
+        return {
+            viewMode: '',
+        }
+    },
+    methods: {
+        change(theEvent) {
+            //change workspace view to content, design, or admin
+            this.viewMode = theEvent
+        }
     }
 }
 </script>
