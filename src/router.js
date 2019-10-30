a024743681a5ae1be49da1e3580b9552461a9ea3
Isaac Ingles <isaac.ingles@gmail.com>
2019-10-30 10:43:32 -0600

    did some lazy loading in router

--- src/router.js
+++ src/router.js
@@ -1,17 +1,89 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import { resolve } from 'path'
 
 import Dashboard from './views/Dashboard.vue'
-import Workspace from './views/Workspace.vue'
+
 //lazy load these when I get the chance
-import Help from './views/Help.vue'
-import About from './views/About.vue'
-import Settings from './views/Settings.vue'
-import Logs from './views/Logs.vue'
+//import Workspace from './views/Workspace.vue'
+//import Help from './views/Help.vue'
+//import About from './views/About.vue'
+//import Settings from './views/Settings.vue'
+//import Logs from './views/Logs.vue'
 import SearchResults from './components/workspace/SearchResults.vue'
 import ModuleEditor from './components/workspace/ModuleEditor.vue'
 import ModuleDetail from './components/workspace/ModuleDetail.vue'
-import WorkspaceViewPort from './components/workspace/WorkspaceViewPort.vue'
+//import WorkspaceViewPort from './components/workspace/WorkspaceViewPort.vue'
+
+/* 
+You can group lazy loading stuff using a third 
+parameter, the groupName, which needs to be
+passed into each routing function.  This makes the 
+file bigger; but you may want to do this in certain 
+situations rather than loading each route one at a time.
+ */
+
+//lazy loading with webpack:
+const Workspace = resolve => {
+  /*
+  takes an array for the first arguement; inside the 
+  array we set up the pathto the component we want to 
+  load.
+
+  The second arguement is another function that calls resolve again
+  */
+  require.ensure(
+      ['./views/Workspace.vue'],
+      () => {
+          resolve(require('./views/Workspace.vue'))
+      }
+  )
+}
+
+const WorkspaceViewPort = resolve => {
+  require.ensure(
+     ['./components/workspace/WorkspaceViewPort.vue'],
+     () => {
+         resolve(require('./components/workspace/WorkspaceViewPort.vue'))
+     }
+ )
+}
+
+const Help = resolve => {
+   require.ensure(
+      ['./views/Help.vue'],
+      () => {
+          resolve(require('./views/Help.vue'))
+      }
+  )
+}
+
+const About = resolve => {
+  require.ensure(
+     ['./views/About.vue'],
+     () => {
+         resolve(require('./views/About.vue'))
+     }
+ )
+}
+
+const Settings = resolve => {
+  require.ensure(
+     ['./views/Settings.vue'],
+     () => {
+         resolve(require('./views/Settings.vue'))
+     }
+ )
+}
+
+const Logs = resolve => {
+  require.ensure(
+     ['./views/Logs.vue'],
+     () => {
+         resolve(require('./views/Logs.vue'))
+     }
+ )
+}
 
 
 Vue.use(Router)
