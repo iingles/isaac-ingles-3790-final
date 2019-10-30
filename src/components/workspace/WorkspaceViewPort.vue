b9fab03bb239f05e46e2b00a90a3396ad5200226
Isaac Ingles <isaac.ingles@gmail.com>
2019-10-30 10:32:52 -0600

    added h1 and h2 to workspaceviewport

--- src/components/workspace/WorkspaceViewPort.vue
+++ src/components/workspace/WorkspaceViewPort.vue
@@ -1,8 +1,12 @@
 <template>
-    <div>
-        content view
-    </div>
-
+    <v-container>
+        <v-row>
+            <h1> Viewing {{this.$route.params.viewmode}}</h1>
+        </v-row>
+        <v-row>
+            <h2>{{this.$route.params.workview}}</h2>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
