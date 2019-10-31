5b3c02207726843810f6547d9c7339c51c544f98
Isaac Ingles <isaac.ingles@gmail.com>
2019-10-31 16:17:00 -0600

    got search table working sorta

--- src/components/workspace/SearchResults.vue
+++ src/components/workspace/SearchResults.vue
@@ -1,138 +1,54 @@
 <template>
   <div>
-    <!-- <transition name="flip" type="animation">
-      <v-card>
-        hi
-      </v-card>
       <v-data-table
+        :v-model="selected"
         :headers="headers"
-        :items="desserts"
+        :items="modules"
         :items-per-page="5"
         class="elevation-1"
         >
         </v-data-table>
-    </transition> -->
-      <!-- <div v-for="mod in mods" :key="mod.id">
+    
+      <!-- <div v-for="mod in modules" :key="mod.id">
        {{ mod.name }}
       </div> -->
   </div>   
 </template>
 
 <script>
-//import contentModules from '../../assets/modules.js'
+import contentModules from '../../assets/modules.js'
 
-//export default {
- // data () {
-   // return {     
-  //     headers: [ 
-  //       {
-  //         text: 'Dessert (100g serving)',
-  //         align: 'left',
-  //         sortable: false,
-  //         value: 'name',
-  //       },
-  //       { text: 'Calories', value: 'calories' },
-  //       { text: 'Fat (g)', value: 'fat' },
-  //       { text: 'Carbs (g)', value: 'carbs' },
-  //       { text: 'Protein (g)', value: 'protein' },
-  //       { text: 'Iron (%)', value: 'iron' },
-  //     ],
-  //     items: [],
-  //     desserts: [
-  //       {
-  //         name: 'Frozen Yogurt',
-  //         calories: 159,
-  //         fat: 6.0,
-  //         carbs: 24,
-  //         protein: 4.0,
-  //         iron: '1%',
-  //       },
-  //       {
-  //         name: 'Ice cream sandwich',
-  //         calories: 237,
-  //         fat: 9.0,
-  //         carbs: 37,
-  //         protein: 4.3,
-  //         iron: '1%',
-  //       },
-  //       {
-  //         name: 'Eclair',
-  //         calories: 262,
-  //         fat: 16.0,
-  //         carbs: 23,
-  //         protein: 6.0,
-  //         iron: '7%',
-  //       },
-  //       {
-  //         name: 'Cupcake',
-  //         calories: 305,
-  //         fat: 3.7,
-  //         carbs: 67,
-  //         protein: 4.3,
-  //         iron: '8%',
-  //       },
-  //       {
-  //         name: 'Gingerbread',
-  //         calories: 356,
-  //         fat: 16.0,
-  //         carbs: 49,
-  //         protein: 3.9,
-  //         iron: '16%',
-  //       },
-  //       {
-  //         name: 'Jelly bean',
-  //         calories: 375,
-  //         fat: 0.0,
-  //         carbs: 94,
-  //         protein: 0.0,
-  //         iron: '0%',
-  //       },
-  //       {
-  //         name: 'Lollipop',
-  //         calories: 392,
-  //         fat: 0.2,
-  //         carbs: 98,
-  //         protein: 0,
-  //         iron: '2%',
-  //       },
-  //       {
-  //         name: 'Honeycomb',
-  //         calories: 408,
-  //         fat: 3.2,
-  //         carbs: 87,
-  //         protein: 6.5,
-  //         iron: '45%',
-  //       },
-  //       {
-  //         name: 'Donut',
-  //         calories: 452,
-  //         fat: 25.0,
-  //         carbs: 51,
-  //         protein: 4.9,
-  //         iron: '22%',
-  //       },
-  //       {
-  //         name: 'KitKat',
-  //         calories: 518,
-  //         fat: 26.0,
-  //         carbs: 65,
-  //         protein: 7,
-  //         iron: '6%',
-  //       },
-  //     ],
-  //   }
-  // },
-  // created() {
-  //   for(let i = 0; i < contentModules.length; i++){
-  //     this.items.push(contentModules[i])
-  //   }
+export default {
+ data () {
+    return {
+      selected: [], 
+      headers: [
+        {
+            text: 'ID',
+            align: 'left',
+            sortable: false,
+            value: 'id',
+          },
+          { text: 'Name', value: 'name' },
+          { text: 'Internal Name', value: 'internalName' },
+          { text: 'Template', value: 'template' },
+          { text: 'Created', value: 'created' },
+          { text: 'Version', value: 'version' },
+      ],
+      modules: contentModules,
+    }
+  },
+  created() {
+    // for(let i = 0; i < contentModules.length; i++){
+    //   this.modules.push(contentModules[i])
+    // }
     
-  //}
-//}
+  }
+}
 </script>
 
 <style scoped>
-  /* .flip-enter-active {
+  .flip-enter-active {
     animation: flipin 1.5s ease-out forwards; 
   }
   .flip-leave-active {
@@ -147,6 +63,6 @@
    @keyframes flipout {
     from {transform: rotateY(180deg);}
     to {transform: rotateY(0deg); }
-  } */
+  }
 
 </style>
