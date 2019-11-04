cc2182a49e2322f5e8d54c62656627cf7ce96c2e
Isaac Ingles <isaac.ingles@gmail.com>
2019-11-04 11:46:08 -0700

    added cards to dashboard page

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,37 @@
 <template>
     <v-container>
-        Dashboard
+        <h1>Dashboard</h1>
+        <h2>Use the lefthand slideout menu to navigate.</h2>
+        <v-row>
+            <!-- 
+            Eventually I could pull all of these dynamically, maybe even
+            make them their own components -- depending on how complicated they get
+             -->
+            <v-col>
+                <v-card class="card-title">
+                    <v-card-title class="light-blue darken-1 white--text" >Alerts</v-card-title>
+                    <v-list>
+                        <v-list-item>Lorem Ipsum: consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</v-list-item>
+                        <v-list-item>Excepteur sint: Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </v-list-item>
+                        <v-list-item>Lorem Ipsum: velit esse cillum dolore eu fugiat nulla pariatur. </v-list-item>
+                        <v-list-item>Ut enim ad minim: consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</v-list-item>
+                    </v-list>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-card class="card-title">
+                    <v-card-title class="light-blue darken-1 white--text">Logs</v-card-title>
+                    <v-list>
+                        <v-list-item>Lorem Ipsum: consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</v-list-item>
+                        <v-list-item>Excepteur sint: Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </v-list-item>
+                        <v-list-item>Lorem Ipsum: velit esse cillum dolore eu fugiat nulla pariatur. </v-list-item>
+                        <v-list-item>Ut enim ad minim: consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</v-list-item>
+                    </v-list>
+                </v-card>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
