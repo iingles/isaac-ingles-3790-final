93b998adc5d35b7a790a8719d2ae37024a0f319b
Isaac Ingles <isaac.ingles@gmail.com>
2019-11-04 15:18:16 -0700

    completed db example

--- src/views/UserManagement.vue
+++ src/views/UserManagement.vue
@@ -5,6 +5,13 @@
         <h1>Remote DB example</h1>
         <v-spacer></v-spacer>
         <v-btn @click="getCourses">Get Courses</v-btn>
+        <v-spacer></v-spacer>
+        <ul>
+            <li v-for="course in courses" :key='course.key'>
+               <p> {{ course.title }} </p>
+               <p> {{ course.prefix }} : {{ course.number }}</p>
+            </li>
+        </ul>
     </v-container>
 </template>
 
@@ -21,12 +28,22 @@ export default {
     methods: {
         getCourses() {
             //might kill the browser if you ask for a ton of data at once
-            axios.get('https://sweltering-fire-2420.firebaseio.com/')
+            axios.get('https://sweltering-fire-2420.firebaseio.com/courses.json')
             .then(response => {
-                console.log(response)
+                // console.log(response.data)
+                const allCoursesObj = response.data 
+                //"e" means entry
+                const objectsToArray = Object.entries(allCoursesObj).map(e => 
+                //this is unique to firebase
+                Object.assign(e[1], {key: e[0]}))
+                //Don't normally need to do this if your server returns normal JSON
+                // console.log(objectsToArray)
+                this.courses = objectsToArray
             })
             .catch(error => console.error(error))
-
+            .finally(()=>{
+                // console.log(this.courses)
+            })
         }
     }
 }
