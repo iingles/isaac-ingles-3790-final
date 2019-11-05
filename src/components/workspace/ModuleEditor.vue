1e99e5386206efb47807b640519f353e7b8e8224
Isaac Ingles <isaac.ingles@gmail.com>
2019-11-04 21:31:43 -0700

    filled in content editor component

--- src/components/workspace/ModuleEditor.vue
+++ src/components/workspace/ModuleEditor.vue
@@ -3,12 +3,40 @@
          <v-card class="pa-10">
             <v-card-title class="mb-10 pa-0 editor-title red--text">Editing {{ this.$route.params.id }}</v-card-title>          
             <div>
-                <p>id: {{ mod.id }}</p>
-                <p>Internal Name: {{ mod.internalName }}</p>
-                <p>Name: {{ mod.name }}</p>
                 <p>Created: {{ mod.created }}</p>
                 <p>Version: {{ mod.version }}</p>
+                <p>ID: {{ mod.id }} </p>
+                <v-checkbox
+                    v-model="changeInternalName"
+                    label="Change Internal Name"
+                >                    
+                </v-checkbox>
+                <v-text-field
+                    :value="mod.internalName"
+                    label="Internal (system) Name"
+                    :disabled="!changeInternalName"
+                    readonly=""
+                ></v-text-field>
+                <v-checkbox
+                    v-model="changeModName"
+                    label="Change Module Name"
+                >                    
+                </v-checkbox>
+                <v-text-field
+                    :value="mod.name"
+                    label="Module Name"
+                    :disabled="!changeModName"
+                    readonly=""
+                ></v-text-field>
+                <v-textarea
+                label="Content Area 1"
+                value="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+                >
+                </v-textarea>
             </div>
+            <v-btn color="red" light>Cancel</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="green" light>Save</v-btn>            
         </v-card>
     </v-container>
 </template>
@@ -21,6 +49,8 @@ export default {
         return {
             modules: contentModules,
             mod: 'test',
+            changeInternalName: false,
+            changeModName: false,
         }
     },
     created() {
