d2c9886213813e49504a3de20934b21be9c6ed5e
Isaac Ingles <isaac.ingles@gmail.com>
2019-11-04 21:05:39 -0700

    pulling data for module editor

--- src/components/workspace/ModuleEditor.vue
+++ src/components/workspace/ModuleEditor.vue
@@ -1,23 +1,38 @@
 <template>
     <v-container>        
          <v-card class="pa-10">
-            <v-card-title class="mb-10 pa-0 editor-title red--text">Editing</v-card-title>          
+            <v-card-title class="mb-10 pa-0 editor-title red--text">Editing {{ this.$route.params.id }}</v-card-title>          
             <div>
-                
+                <p>id: {{ mod.id }}</p>
+                <p>Internal Name: {{ mod.internalName }}</p>
+                <p>Name: {{ mod.name }}</p>
+                <p>Created: {{ mod.created }}</p>
+                <p>Version: {{ mod.version }}</p>
             </div>
         </v-card>
     </v-container>
 </template>
 
 <script>
-
+import contentModules from '../../assets/modules.js'
 
 export default {
     data: () => {
         return {
-            
+            modules: contentModules,
+            mod: 'test',
         }
-    },    
+    },
+    created() {
+       let vm = this
+
+       for(let i = 0; i < vm.modules.length; i++) {
+            if(vm.modules[i].internalName == this.$route.params.id) {
+                vm.mod = vm.modules[i]
+                break
+            }
+       }
+    }
 }
 </script>
 
