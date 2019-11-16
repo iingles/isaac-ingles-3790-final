b5eaa54056fc9240e785d24288cc4d5431cb5f35
Isaac Ingles <isaac.ingles@gmail.com>
2019-11-16 13:24:23 -0700

    added an html field to templates

--- src/assets/templates.js
+++ src/assets/templates.js
@@ -11,28 +11,28 @@ var designTemplates = [
         name: 'article',
         version: 1,
         modified: '11/2/2019',
-        html: '<div>$mainParagraph</div>'
+        html: '<div>$secondParagraph</div>'
     },
     {
         id: 3,
         name: 'media-video',
         version: 1,
         modified: '11/2/2019',
-        html: '<div>$mainParagraph</div>'
+        html: '<div>$thirdParagraph</div>'
     },
     {
         id: 4,
         name: 'media-gallery',
         version: 1,
         modified: '11/2/2019',
-        html: '<div>$mainParagraph</div>'
+        html: '<div>$fourthParagraph</div>'
     },
     {
         id: 5,
         name: 'newsPage',
         version: 1,
         modified: '11/2/2019',
-        html: '<div>$mainParagraph</div>'
+        html: '<div>$fifthParagraph</div>'
     },
 ]
 
