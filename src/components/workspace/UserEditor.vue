2a49ae79984ad6800ef85240b38f570661042cb6
Isaac Ingles <isaac.ingles@gmail.com>
2019-11-27 16:23:34 -0700

    added more info to change in user profile

--- src/components/workspace/UserEditor.vue
+++ src/components/workspace/UserEditor.vue
@@ -2,7 +2,7 @@
     <v-card>
         <v-card-title class="mb-10 pa-0 editor-title red--text">Editing {{ user.name + ' ' + user.surname }}</v-card-title>
        <v-row>
-           <v-col>
+           <v-col cols='6'>
                <v-img 
                 :src="user.photo"
                 max-height=500
@@ -10,6 +10,48 @@
                 > 
                 </v-img>
             </v-col>
+            <v-col cols='6'>
+                <p>Created: 11/27/2019 (static)</p>
+                <p>ID: {{ user.email.split('@')[0] }} </p>
+                <v-checkbox
+                    v-model="user.email"
+                    label="Change ID"
+                >  
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols='12'>
+                <v-checkbox
+                v-model="user.name"
+                label="Change First Name"
+                >                    
+            </v-checkbox>
+            <v-text-field
+                :value="user.name"
+                label="First Name"
+                :disabled="!changeUserFirstName"
+            ></v-text-field>
+            <v-checkbox
+                v-model="user.surname"
+                label="Change User Surname"
+            >                    
+            </v-checkbox>
+            <v-text-field
+                :value="user.surname"
+                label="Last Name"
+                :disabled="!changeUserSurname"
+            ></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-btn 
+            color="red" 
+            light
+            @click="confirmCancel(user.id)"
+            >Cancel</v-btn>
+            <v-btn 
+            color="green"
+            light @click="confirmSave(mod.id)">Save</v-btn>  
         </v-row>
     </v-card>
 </template>
@@ -26,6 +68,8 @@ export default {
     data: ()=> {
         return {
             user: {},
+            changeUserFirstName: false,
+            changeUserSurname: false,
         }
     },
     created() {
