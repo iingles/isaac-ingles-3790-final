57bb4ad1c9ce554e91f0f4096ece289d9eab3a70
Isaac Ingles <isaac.ingles@gmail.com>
2019-11-27 13:55:59 -0700

    added async imports to master editor

--- src/components/workspace/EditorMaster.vue
+++ src/components/workspace/EditorMaster.vue
@@ -1,10 +1,25 @@
 <template>
-        
+    <v-col>
+        <v-container>
+            <!-- Dynamic component -->
+            <compnent :is="activeEditor"> </component>
+        </v-container>
+    </v-col>
 </template>
 
 <script>
+
 export default {
-    
+    components: {
+        //Load components asynchronously (import returns a promise)
+        'ModuleEditor': () => import('./ModuleEditor.vue'),
+        'UserEditor': () => import('./UserEditor.vue')
+    },
+    data: ()=> {
+        return {
+            activeEditor: ''
+        }
+    }
 }
 </script>
 
