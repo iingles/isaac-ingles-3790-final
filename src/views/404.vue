686d3fa58455ee56099ebfaa2143d8a8477bf603
Isaac Ingles <isaac.ingles@gmail.com>
2019-12-06 16:45:38 -0700

    added 404 page

---
+++ src/views/404.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container>
+        <h1>404</h1>
+        <p>Sorry, the requested page cannot be found.</p>
+    </v-container>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
