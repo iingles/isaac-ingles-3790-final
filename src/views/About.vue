f64ead18dc49db423a011f16db79422d8f73f937
Isaac Ingles <isaac.ingles@gmail.com>
2019-12-06 17:25:48 -0700

    added content to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,8 +2,22 @@
   <div class="about">
     <v-card class="ma-10 pb-5 pl-5 pr-5 pt-5">
       <v-card-title class="blue lighten-1 white--text mb-5">About this platform</v-card-title>
-      <p>Version 0.0.0</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h1>RIA 1 Final Project</h1>
+      <h6>Version 1.0.0 -- 12/9/2019</h6>
+      <p>This is a prototype CMS interface designed as a final project for Rich Internet Applications I at Utah Valley University.</p>
     </v-card>
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+h6 {
+  margin-bottom: 20px;
+}
+
+</style>
